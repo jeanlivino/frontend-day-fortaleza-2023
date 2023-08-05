e0cc87fda925490c3d38c720d3018bd6c51ec74a
Jean Livino <jeanlivino@gmail.com>
2023-08-05 14:17:44 -0300

    fix: open link in new tab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,10 +15,13 @@ export default function Home() {
       />
       <h1 style={{ color: '#fff' }}>Em breve</h1>
       <div className={styles.icons}>
-        <a href='https://www.instagram.com/frontendce/'>
+        <a href='https://www.instagram.com/frontendce/' target='_blank'>
           <Image src='/instagram.svg' alt='Instagram' width={24} height={24} />
         </a>
-        <a href='https://chat.whatsapp.com/KjuHQ55RV6FBERixk5eg6g'>
+        <a
+          href='https://chat.whatsapp.com/KjuHQ55RV6FBERixk5eg6g'
+          target='_blank'
+        >
           <Image src='/whatsapp.svg' alt='WhatsApp' width={24} height={24} />
         </a>
       </div>
