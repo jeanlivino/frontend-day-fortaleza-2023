1dd65c62d8a9878ceb54016d99282c0946d89780
Jean Livino <jeanlivino@gmail.com>
2023-08-16 19:35:35 -0300

    feat: add pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 import 'normalize.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -16,6 +17,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang='pt-br'>
+      <Script
+        id='fb-pixel'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '226302154877929');
+fbq('track', 'PageView');
+`,
+        }}
+      />
       <body className={inter.className}>{children}</body>
     </html>
   );
