9744003ae5bcac6d8ebf2650933ec69dbc9c1cd5
Jean Livino <jeanlivino@gmail.com>
2023-08-18 14:42:12 -0300

    feat: addconsole

--- src/services/site.ts
+++ src/services/site.ts
@@ -5,5 +5,8 @@ import { cache } from 'react';
 export const revalidate = 30;
 
 export const getSiteData = cache(() =>
-  dataApi.get('site/v1/data').then((res) => res.data as SiteData)
+  dataApi.get('site/v1/data').then((res) => {
+    console.log(res.data);
+    return res.data as SiteData;
+  })
 );
