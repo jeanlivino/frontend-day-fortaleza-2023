2a3fee7eca4f5550dcc0889fab8ac29b5e415aa5
Jean Livino <jeanlivino@gmail.com>
2023-08-19 16:24:24 -0300

    feat: loading to ticket section

--- src/sections/TicketSection/index.tsx
+++ src/sections/TicketSection/index.tsx
@@ -13,25 +13,33 @@ const TicketSection: React.FC = () => {
           fontSize='2xl'
           fontWeight='bold'
           textAlign='center'
+          mb='4'
         >
           Inscrições
         </styled.h2>
-        {/* <styled.p color='primary' mt='4' fontSize='md' textAlign='center'>
+        <Box
+          minH='200px'
+          bg='url(/images/loading.gif) white'
+          backgroundPosition='center'
+          backgroundRepeat='no-repeat'
+          bgSize='80px 80px'
+        >
+          {/* <styled.p color='primary' mt='4' fontSize='md' textAlign='center'>
           As inscrições irão iniciar em breve!
         </styled.p> */}
-        <div id='eventbrite-widget-container-699928714627'></div>
+          <div id='eventbrite-widget-container-699928714627'></div>
 
-        <Script
-          strategy='beforeInteractive'
-          src='https://www.eventbrite.com.br/static/widgets/eb_widgets.js'
-        ></Script>
+          <Script
+            strategy='beforeInteractive'
+            src='https://www.eventbrite.com.br/static/widgets/eb_widgets.js'
+          ></Script>
 
-        <Script
-          type='text/javascript'
-          id='eventbrite-widget-js'
-          strategy='lazyOnload'
-          dangerouslySetInnerHTML={{
-            __html: `
+          <Script
+            type='text/javascript'
+            id='eventbrite-widget-js'
+            strategy='lazyOnload'
+            dangerouslySetInnerHTML={{
+              __html: `
     window.EBWidgets.createWidget({
         widgetType: 'checkout',
         eventId: '699928714627',
@@ -39,8 +47,9 @@ const TicketSection: React.FC = () => {
         iframeContainerHeight: 625,
     });
     `,
-          }}
-        ></Script>
+            }}
+          ></Script>
+        </Box>
       </Container>
     </Box>
   );
