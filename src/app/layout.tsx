50ebbe9819116ba682f3e6bfb23a011f3b70f381
Jean Livino <jeanlivino@gmail.com>
2023-09-15 11:05:02 -0300

    feat: microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,20 @@ export default function RootLayout({
         <Script
           id='fb-pixel'
           strategy='afterInteractive'
+          type='text/javascript'
+          dangerouslySetInnerHTML={{
+            __html: `
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "ivkpbyfsly");
+`,
+          }}
+        />
+        <Script
+          id='fb-pixel'
+          strategy='afterInteractive'
           dangerouslySetInnerHTML={{
             __html: `
 !function(f,b,e,v,n,t,s)
