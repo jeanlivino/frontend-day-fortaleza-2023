9663bb0e35da06a013ad453b5d1b013390b63a1c
Jean Livino <jean.livino@ifood.com.br>
2023-10-03 13:18:25 -0300

    feat: carousel to show winners

--- src/app/do-draw/page.tsx
+++ src/app/do-draw/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 import DoDrawForm from '@/components/DoDrawForm';
+import WinnersList from '@/components/WinnersList';
 import { Box, Center, styled } from '@/styled-system/jsx';
 import { DrawItem } from '@/types';
 import React from 'react';
 
 const DoDrawPage: React.FC = () => {
   const [winners, setWinners] = React.useState<DrawItem[]>([]);
+
+  if (winners.length > 0) return <WinnersList winners={winners} />;
 
   return <DoDrawForm onDraw={setWinners} />;
 };
